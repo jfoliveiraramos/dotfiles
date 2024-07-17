3885a87245e2ff4abaca51fbc9fa1a3e3e83cbda
João Ramos <1joaoramos1@gmail.com>
2024-07-17 21:57:53 +0100

    Updates NeoVim config

--- upload_nvim.sh
+++ upload_nvim.sh
@@ -1,4 +1,14 @@
-#/etc/bin/bash!
+#!/bin/bash
+
+if [[ $# -eq 0 ]]; then
+    commit="Updates NeoVim config"
+else
+    commit="$1"
+fi
 
 rm -rf ./.config/nvim
 cp -r ~/.config/nvim ./.config/
+
+git add -A
+git commit -a -m "$commit"
+git push
