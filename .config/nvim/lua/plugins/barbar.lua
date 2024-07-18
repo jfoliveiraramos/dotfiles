3add7ca47a74e6af17d2251403cea02e2048cd04
João Ramos <1joaoramos1@gmail.com>
2024-07-18 14:27:56 +0100

    Fixed Rust-Analyzer

--- .config/nvim/lua/plugins/barbar.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "romgrk/barbar.nvim",
-    keys = {
-      { "<A-,>", "<Cmd>BufferPrevious<CR>", { noremap = true, silent = true } },
-      { "<A-.>", "<Cmd>BufferNext<CR>", { noremap = true, silent = true } },
-    },
-  },
-}
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,14 +1,14 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    keys = {
-      {
-        "<leader>pf",
-        function()
-          require("telescope.builtin").find_files()
-        end,
-        desc = "Find Plugin File",
-      },
+    {
+        "nvim-telescope/telescope.nvim",
+        keys = {
+            {
+                "<leader>pf",
+                function()
+                    require("telescope.builtin").find_files()
+                end,
+                desc = "Find Plugin File",
+            },
+        },
     },
-  },
 }
