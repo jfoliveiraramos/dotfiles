f9099aef42dabd0ad888d4c5e76bb5b58c07872d
João Ramos <1joaoramos1@gmail.com>
2024-12-15 11:06:02 +0100

    Updates waybar script

--- .config/scripts/popup_waybar.sh
+++ .config/scripts/popup_waybar.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-# Launch Waybar temporarily
-killall waybar
-echo "Starting Waybar..."
-waybar &      # Launch Waybar in the background
-WAYBAR_PID=$! # Capture the process ID of Waybar
+pid=$(pgrep -x "waybar")
 
-# Keep Waybar open for a specific duration (e.g., 5 seconds)
-DISPLAY_DURATION=5
-echo "Waybar will be visible for $DISPLAY_DURATION seconds."
-sleep $DISPLAY_DURATION
-
-# Kill Waybar process
-echo "Stopping Waybar..."
-kill $WAYBAR_PID
-
-echo "Waybar has been closed."
+if [ -n "$pid" ]; then
+    kill "$pid"
+else
+    waybar &
+    WAYBAR_PID=$!
+    DISPLAY_DURATION=5
+    sleep $DISPLAY_DURATION
+    kill $WAYBAR_PID
+fi
