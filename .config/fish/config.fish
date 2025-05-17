a3a406f3d9c5759ed6b864e64d55f02b860e68ac
João Ramos <1joaoramos1@gmail.com>
2025-05-17 10:43:37 +0200

    Hyprlock tune

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,9 @@ end
 
 set -g fish_greeting
 
+set -x GOROOT /usr/local/go
+set -x PATH $GOROOT/bin $PATH
+
 set -x STARSHIP_CONFIG $HOME/.config/starship/starship.toml
 starship init fish | source
 zoxide init --cmd cd fish | source
