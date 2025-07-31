92b5ba43d3192d29d51ad5e14ebc415f7721b96f
João Ramos <1joaoramos1@gmail.com>
2025-07-31 07:17:26 +0200

    Updates config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,6 +7,10 @@ set -g fish_greeting
 set -x GOPATH $HOME/.go
 set -x PATH $PATH $GOPATH/bin
 
+set -x PATH $PATH $HOME/.cache/rebar3/bin
+
+set -x QT_QPA_PLATFORM xcb
+
 set -x STARSHIP_CONFIG $HOME/.config/starship/starship.toml
 starship init fish | source
 zoxide init --cmd cd fish | source
@@ -17,7 +21,7 @@ bind \ca tmux attach
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 if test -f /home/ramos/.miniconda3/bin/conda
-    eval /home/ramos/.miniconda3/bin/conda "shell.fish" "hook" $argv | source
+    eval /home/ramos/.miniconda3/bin/conda "shell.fish" hook $argv | source
 else
     if test -f "/home/ramos/.miniconda3/etc/fish/conf.d/conda.fish"
         . "/home/ramos/.miniconda3/etc/fish/conf.d/conda.fish"
@@ -27,10 +31,9 @@ else
 end
 # <<< conda initialize <<<
 
-
 # pnpm
 set -gx PNPM_HOME "/home/joaoramos/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
