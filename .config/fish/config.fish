a289ca3454f4a811600d2e81f7f600fa5c6057cd
João Ramos <1joaoramos1@gmail.com>
2025-12-03 10:00:30 +0100

    Updates mamba config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -42,5 +42,5 @@ end
 # !! Contents within this block are managed by 'micromamba shell init' !!
 set -gx MAMBA_EXE /usr/bin/micromamba
 set -gx MAMBA_ROOT_PREFIX "/home/ramos/.micromamba"
-$MAMBA_EXE shell hook --shell fish --root-prefix $MAMBA_ROOT_PREFIX | source
+# $MAMBA_EXE shell hook --shell fish --root-prefix $MAMBA_ROOT_PREFIX | source
 # <<< mamba initialize <<<
